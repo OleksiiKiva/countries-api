32ba311c1bff4dfb8caad6072f232e6049374294
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-07-15 17:26:08 +0300

    feat: implement init & __get_data method

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+from typing import Dict, Any
+from urllib.parse import urljoin
+
+import requests
+
+BASE_URL = "https://restcountries.com/v3.1/all"
+HOME_URL = urljoin(BASE_URL, "?fields=name,capital,flags")
+
+
 class CountryAPI:
     """A class that interacts with the API on https://restcountries.com"""
-    pass
+
+    def __init__(self):
+        self.url = HOME_URL
+
+    def __get_data(self) -> Dict[str, Any]:
+        """A method that receives data from a third party API and converts it to json"""
+
+        try:
+            response = requests.get(url=self.url)
+            if not response:
+                print(
+                    "The web-address was not entered or was entered incorrectly. Try again"
+                )
+            else:
+                return response.json()
+
+        except requests.ConnectionError:
+            print("Connection error")
+
+        except requests.Timeout:
+            print("Timeout error")
+
+        except requests.RequestException:
+            print("Request error")
