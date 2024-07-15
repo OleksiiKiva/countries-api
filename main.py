0201e7eecd105f6795f7e9119c863a1b59d3e911
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-07-15 17:27:32 +0300

    feat: implement & __parse_data method

--- main.py
+++ main.py
@@ -33,3 +33,17 @@ class CountryAPI:
 
         except requests.RequestException:
             print("Request error")
+
+    def __parse_data(self) -> list[list]:
+        """A method that receives data in json format and converts it into a nested list"""
+
+        if self.__get_data():
+            result = self.__get_data()
+            list_for_table = []
+            for country in result:
+                name = country.get("name", {}).get("common", {})
+                capital = ", ".join(country.get("capital", {}))
+                flags = country.get("flags", {}).get("png", {})
+                list_for_table.append([name, capital, flags])
+
+            return list_for_table
